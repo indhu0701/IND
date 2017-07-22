0f26bb5c7198f9bc0da4a640363639f0f5a1579e
indhu0701 <30342676+indhu0701@users.noreply.github.com>
2017-07-22 14:26:13 +0530

    Create 5.C

---
+++ 5.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+inta,b,c;
+printf("enter the numbers");
+scanf("%d%d%d",&a&b&c);
+if(a>b&&a>c)
+printf("%d largest no,a");
+elseif(b>c)
+printf("%d largest no,b");
+else
+printf("%d largest no,c");
+}
